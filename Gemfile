c093ac1a5b4147fa65744277a17086a1eb7ea5b0
vagrant <vagrant@localhost.localdomain>
2019-02-11 06:04:46 +0000

    category_form

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.0'
+gem 'kaminari'
 gem 'simple_form'
 gem 'devise'
 gem 'devise-bootstrap-views'
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
+    @books = Book.order('title').page(params[:page])
   end
 
   # GET /books/1
@@ -69,6 +69,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:title, :author)
+      params.require(:book).permit(:title, :author, category_ids: [])
     end
 end
